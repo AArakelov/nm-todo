2cbaef89b1eb784446f297bfd0b5dbe64c36cc4a
Armen Arakelov <a.g.arakelov@gmail.com>
2025-04-21 19:57:56 +0200

    feat(todo): add routing, header, and Todo interface implementation
    - Configured routes for 'add', 'list', and 'favorite' pages with default redirection.
    - Integrated global `HeaderComponent` and `router-outlet` in the app component.
    - Updated TypeScript path aliases for cleaner module resolution.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'add',
+    loadComponent: () =>
+      import('@pages/page-add-todo/page-add-todo.component').then(m => m.PageAddTodoComponent),
+  },
+  {
+    path: 'list',
+    loadComponent: () =>
+      import('@pages/page-todo-list/page-todo-list.component').then(m => m.PageTodoListComponent),
+  },
+  {
+    path: 'favorite',
+    loadComponent: () =>
+      import('@pages/page-todo-list/page-todo-list.component').then(m => m.PageTodoListComponent),
+  },
+  {path: '**', redirectTo: 'list'},
+];
