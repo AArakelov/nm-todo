a9f9cb4af57cf2ad3b1d2d7dcb5fa42e507f9c2a
Armen Arakelov <a.g.arakelov@gmail.com>
2025-04-22 00:33:14 +0200

    refactor(todo): enhance Todo List page with improved UI and logic
    - Replaced default template with dynamic layout separating favorite, today, and other tasks.
    - Introduced computed properties for clear and efficient state management.
    - Applied updated styles for better responsiveness and visual consistency.

--- src/app/pages/page-todo-list/page-todo-list.component.ts
+++ src/app/pages/page-todo-list/page-todo-list.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, computed, inject} from '@angular/core';
+import {TodoService}                                          from '@features/todo/data-access/todo.service';
+import {Router}                                               from '@angular/router';
+import {TodoItemComponent}                                    from '@features/todo/ui/todo-item/todo-item.component';
 
 @Component({
   selector: 'app-page-todo-list',
-  imports: [],
+  imports: [
+    TodoItemComponent
+  ],
   templateUrl: './page-todo-list.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrl: './page-todo-list.component.scss'
 })
 export class PageTodoListComponent {
 
+  filteredTodos = computed(() => {
+    const todos = this.todos();
+    return this.isFavoriteRoute() ? todos.filter(t => t.isFavorite) : todos;
+  });
+  otherTodos = computed(() => {
+    const now = new Date().toDateString();
+    return this.filteredTodos().filter(t => new Date(t.expirationDate).toDateString() !== now);
+  });
+  todayTodos = computed(() => {
+    const now = new Date().toDateString();
+    return this.filteredTodos().filter(t => new Date(t.expirationDate).toDateString() === now);
+  });
+  private readonly todoService = inject(TodoService);
+  todos = this.todoService.todos;
+  private router = inject(Router);
+  isFavoriteRoute = computed(() => this.router.url.includes('favorite'));
 }
